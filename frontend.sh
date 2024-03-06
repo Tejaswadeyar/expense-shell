19a66f30527893f890a0799497442e94b2d0c793
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-03-06 19:01:15 +0530

    Created shell script for frontend

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,7 @@ systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+# shellcheck disable=SC2164
+cd  /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl restart nginx
