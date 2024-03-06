d468e62299d23384b107dcbe85e6a2f9efd7b17a
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-03-06 21:19:18 +0530

    Created shell script for frontend

--- backend.sh
+++ backend.sh
@@ -2,17 +2,17 @@ dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
 dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
 
+useradd expense
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
 
-cd /app
 npm install
 
-cp backend.service /etc/systemd/system/backend.service
 
 systemctl daemon-reload
 
