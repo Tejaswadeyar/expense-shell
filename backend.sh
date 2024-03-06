780217396fe3f364b8a25bfa13125d9f0861c34b
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-03-06 21:26:24 +0530

    Created shell script for frontend

--- backend.sh
+++ backend.sh
@@ -5,8 +5,9 @@ dnf install nodejs -y
 cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
-mkdir /app
 
+rm -rf /app
+mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
