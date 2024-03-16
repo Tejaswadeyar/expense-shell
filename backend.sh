9e9b36e7e79f35313d77bdae6ee4457d18a9e0bf
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-03-16 18:37:17 +0530

    shell code function

--- backend.sh
+++ backend.sh
@@ -13,32 +13,32 @@ Head " install the nodejs"
 dnf install nodejs -y &>>$log_file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
-Head " add username"
+Head "add username"
 useradd expense &>>$log_file
 
-Head " remove app folder"
+Head "remove app folder"
 rm -rf /app &>>$log_file
 
-Head " create app"
+Head "create app"
 mkdir /app &>>$log_file
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app &>>$log_file
 unzip /tmp/backend.zip &>>$log_file
 
 
-Head " intall npm"
+Head "intall npm"
 npm install &>>$log_file
 
 
 systemctl daemon-reload &>>$log_file
 
-Head "  enable backend"
+Head "enable backend"
 systemctl enable backend &>>$log_file
 
-Head "  restart backend"
+Head "restart backend"
 systemctl restart backend &>>$log_file
 
-Head " install mysql"
+Head "install mysql"
 dnf install mysql -y &>>$log_file
 
 mysql -h mysql-dev.tejas1996.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
