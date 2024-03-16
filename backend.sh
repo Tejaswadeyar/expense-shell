ef8ce56a44c6d6c36a9875b301fd3f1ca293d56f
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-03-16 18:14:28 +0530

    shell code function

--- backend.sh
+++ backend.sh
@@ -1,39 +1,43 @@
 MYSQL_PASSWORD=${1}
 log_file=/tmp/expense.log
-echo -e "\e[34m disable the default version\e[0m"
+
+Head() {
+  echo -e "\e[35m$2\e[om"
+
+Head " disable the default version"
 dnf module disable nodejs -y &>>$log_file
 dnf module enable nodejs:18 -y &>>$log_file
 
-echo -e "\e[34m install the nodejs\e[0m"
+Head " install the nodejs"
 dnf install nodejs -y &>>$log_file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
-echo -e "\e[34m add username\e[0m"
+Head " add username"
 useradd expense &>>$log_file
 
-echo -e "\e[34m remove app folder\e[0m"
+Head " remove app folder"
 rm -rf /app &>>$log_file
 
-echo -e "\e[34m create app\e[0m"
+Head " create app"
 mkdir /app &>>$log_file
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app &>>$log_file
 unzip /tmp/backend.zip &>>$log_file
 
 
-echo -e "\e[34m intall npm\e[0m"
+Head " intall npm"
 npm install &>>$log_file
 
 
 systemctl daemon-reload &>>$log_file
 
-echo -e "\e[34m  enable backend\e[0m"
+Head "  enable backend"
 systemctl enable backend &>>$log_file
 
-echo -e "\e[34m  restart backend\e[0m"
+Head "  restart backend"
 systemctl restart backend &>>$log_file
 
-echo -e "\e[34m install mysql\e[0m"
+Head " install mysql"
 dnf install mysql -y &>>$log_file
 
 mysql -h mysql-dev.tejas1996.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
