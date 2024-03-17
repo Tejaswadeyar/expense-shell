75a9c779cdf5879646da7fef7c2c2ecb32355a12
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-03-17 12:22:31 +0530

    shell code function

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,33 @@
+log_file=/tmp/expense.log
+
+Head() {
+  echo -e "\e[36m$1\e[0m"
+  }
+
+
+ Head"install nginic"
 dnf install nginx -y
+echo $?
+
+Head"copy the .conf to defult expense.conf"
 cp expense.conf /etc/nginx/default.d/expense.conf
+echo $?
 
+Head"remove default htlm code"
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd  /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo $?
 
+Head"download Application code"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo $?
+
+cd  /usr/share/nginx/html
+
+Heead"extract the frontend.zip file"
+unzip /tmp/frontend.zip
+echo $?
+
+Head"start the nginx service"
 systemctl enable nginx
 systemctl restart nginx
+echo $?
